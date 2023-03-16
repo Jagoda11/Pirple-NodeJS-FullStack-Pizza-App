a69350669da157f7326c81a1bdecc512376f8c06
Jagoda11 <31731113+Jagoda11@users.noreply.github.com>
2023-03-16 14:09:58 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   hashingSecret: "password",
   mail: {
-    apiKey: "5fc23252e8577b9f8c4ae4b94032ba84-d32d817f-c569a575",
-    domain: "sandbox11695060370a437689df318023f004f1.mailgun.org",
+    apiKey: "5fc23252e8577b9f8c4ae4b94032ba84-d32d817f-c569a57523",
+    domain: "sandbox11695060370a437689df318023f004f761.mailgun.org",
     from:
-      "Mailgun Sandbox <postmaster@sandbox11695060370a437689df318023f004f1.mailgun.org>",
+      "Mailgun Sandbox <postmaster@sandbox11695060370a437689df318023f004f761.mailgun.org>",
   },
   stripe: {
     apiKey:
